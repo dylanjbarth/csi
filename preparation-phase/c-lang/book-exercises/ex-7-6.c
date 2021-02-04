213b9016e87faa7e4ade2bea7493547da06eb098
Dylan Barth <dylan.barth@liongard.com>
2021-02-04 06:41:51 -0600

    Finish ex-7-6

--- preparation-phase/c-lang/book-exercises/ex-7-6.c
+++ preparation-phase/c-lang/book-exercises/ex-7-6.c
@@ -1,6 +1,9 @@
 // Exercise 7 - 6. Write a program to compare two files, printing the first line where they differ.
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAXLINE 1000
 
 int main(int argc, char *argv[])
 {
@@ -11,6 +14,20 @@ int main(int argc, char *argv[])
   }
   FILE *fp1 = fopen(argv[1], "r");
   FILE *fp2 = fopen(argv[2], "r");
-  char *line1, *line2;
-  while (line1 = fgets(fp1))
+  char *line1 = malloc(MAXLINE * (sizeof(char)));
+  char *line2 = malloc(MAXLINE * (sizeof(char)));
+  int i = 0;
+  while ((fgets(line1, MAXLINE, fp1) != NULL) && (fgets(line2, MAXLINE, fp2)) != NULL)
+  {
+    if (strcmp(line1, line2) != 0)
+    {
+      printf("Found differing line:\n");
+      printf("File 1, line %d: %s\n", i, line1);
+      printf("File 2, line %d: %s\n", i, line2);
+      exit(0);
+    }
+    i++;
+  }
+  printf("These files are the same.\n");
+  exit(0);
 }
