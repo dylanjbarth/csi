eee119a7edb72b37bf5ef4ae9d2987ff8d276e1b
Dylan Barth <dylan.barth@liongard.com>
2021-02-04 20:45:00 -0600

    Printing out some stat output.

--- preparation-phase/c-lang/ls-clone/ls-clone.c
+++ preparation-phase/c-lang/ls-clone/ls-clone.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <sys/stat.h>
 
 #define PROGRAM "ls-clone"
+#define MAXFILENAME 100
+
+int is_file(struct stat *f);
+int is_dir(struct stat *f);
+void print_dir_or_file(char *s);
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +14,42 @@ int main(int argc, char *argv[])
   printf("N args %d\n", argc);
   while (--argc > 0)
   {
-    printf("Arg %s\n", *++argv);
+    // If a file, just print the file
+    // TODO some arg parsing first.
+    char *cur_arg = *++argv;
+    printf("Arg %s\n", cur_arg);
+    print_dir_or_file(cur_arg);
+  }
+}
+
+// See https://man7.org/linux/man-pages/man7/inode.7.html
+int is_file(struct stat *f)
+{
+  return S_ISREG(f->st_mode);
+}
+int is_dir(struct stat *f)
+{
+  return S_ISDIR(f->st_mode);
+}
+
+void print_dir_or_file(char *dir_or_file)
+{
+  struct stat statbuf;
+  int stat_result = stat(dir_or_file, &statbuf);
+  if (stat_result == -1)
+  {
+    printf("Unable to open %s.\n", dir_or_file);
+  }
+  else if (is_file(&statbuf))
+  {
+    printf("%s is a file.\n", dir_or_file);
+  }
+  else if (is_dir(&statbuf))
+  {
+    printf("%s is a directory.\n", dir_or_file);
+  }
+  else
+  {
+    printf("File type of %s is not supported by %s.\n", dir_or_file, PROGRAM);
   }
 }
