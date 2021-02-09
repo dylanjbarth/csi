b2df90b70f7e9c24456efb84e3b4c3f37c06d0ba
Dylan Barth <dylan.barth@liongard.com>
2021-02-08 21:46:21 -0600

    Bit of cleanup on strcmp.

--- preparation-phase/c-lang/ls-clone/sorting.c
+++ preparation-phase/c-lang/ls-clone/sorting.c
@@ -1,14 +1,14 @@
 #include "ls-clone.h"
 
+// These sorting functions should return 0 if the strings are equal, positive if item 2 is greater than item 1 or negative if item 2 is less than item 1.
 int sort_lexagraphic(struct dirfile *df1, struct dirfile *df2)
 {
-  int res = strcmp(df2->filename, df1->filename);
-  return res;
+  return strcmp(df2->filename, df1->filename);
 }
 
 int sort_size(struct dirfile *df1, struct dirfile *df2)
 {
-  return df1->s.st_size - df2->s.st_size;
+  return df2->s.st_size - df1->s.st_size;
 }
 
 int sort_no_op(struct dirfile *df1, struct dirfile *df2)
