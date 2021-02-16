aa5d4512fa78f988117635b0a826474b501b7118
Dylan Barth <dylan.barth@liongard.com>
2021-02-15 19:15:26 -0600

    Some exercises from ch 1

---
+++ preparation-phase/golang/code/src/gopl-exercises/ex-1-9/main.go
@@ -0,0 +1,38 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io"
+	"strings"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if (!strings.HasPrefix("http://", url)) {
+			url = "http://" + url;
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Fprintf(os.Stdout, "Status code %s\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		resp.Body.Close()
+	}
+}
+
+//!-
