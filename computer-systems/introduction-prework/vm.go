6b6edaf1f2ee0f2f5b380599487f501c7e9aeb52
Dylan Barth <dylan.barth@liongard.com>
2021-03-22 19:41:15 -0500

    clean up vm code.

--- computer-systems/introduction-prework/vm.go
+++ computer-systems/introduction-prework/vm.go
@@ -20,11 +20,6 @@ const (
 	Beqz = 0x08
 )
 
-const (
-	r1 = 0x01
-	r2 = 0x02
-)
-
 // Given a 256 byte array of "memory", run the stored program
 // to completion, modifying the data in place to reflect the result
 //
@@ -52,38 +47,20 @@ func compute(memory []byte) {
 		switch op {
 		case Load:
 			fmt.Printf("Load %d %d\n", oparg1, oparg2)
-			if oparg1 == r1 {
-				registers[1] = memory[oparg2]
-			} else if oparg1 == r2 {
-				registers[2] = memory[oparg2]
-			} else {
-				panic(fmt.Errorf("unexpected register value: %v", oparg1))
-			}
+			registers[oparg1] = memory[oparg2]
 		case Store:
 			fmt.Printf("Store %d %d\n", oparg1, oparg2)
-			if oparg1 == r1 {
-				memory[oparg2] = registers[1]
-			} else if oparg1 == r2 {
-				memory[oparg2] = registers[2]
-			} else {
-				panic(fmt.Errorf("unexpected register value: %v", oparg1))
-			}
+			memory[oparg2] = registers[oparg1]
 		case Add:
 			fmt.Printf("Add %d %d\n", oparg1, oparg2)
-			registers[1] = registers[1] + registers[2]
+			registers[1] += registers[2]
 		case Sub:
 			fmt.Printf("Subtract %d %d\n", oparg1, oparg2)
-			if oparg1 == r1 && oparg2 == r2 {
-				registers[1] = registers[1] - registers[2]
-			} else if oparg1 == r2 && oparg2 == r1 {
-				registers[1] = registers[2] - registers[1]
-			} else {
-				panic(fmt.Errorf("one or more unexpected register values: %v, %v", oparg1, oparg2))
-			}
+			// Possible bug? Could oparg1 ever be r2 for some reason?
+			registers[oparg1] -= registers[oparg2]
 		case Halt:
 			fmt.Println("Halt")
 			return
 		}
-		// fmt.Printf("registers: pc: %d r1: %d r2: %d\n", registers[0], registers[1], registers[2])
 	}
 }
