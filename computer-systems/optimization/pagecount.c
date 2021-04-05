6a7b01c49829d49925bf58d3f6115287c04b03ac
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 07:26:10 -0500

    Use bit shifting to speed up by >50%.

--- computer-systems/optimization/pagecount.c
+++ computer-systems/optimization/pagecount.c
@@ -4,20 +4,26 @@
 
 #define TEST_LOOPS 10000000
 
-uint64_t pagecount(uint64_t memory_size, uint64_t page_size)
+uint64_t pagecount(uint64_t memory_size, uint8_t page_size)
 {
-  return memory_size / page_size;
+  return memory_size >> page_size;
 }
 
 int main(int argc, char **argv)
 {
   clock_t baseline_start, baseline_end, test_start, test_end;
   uint64_t memory_size, page_size;
+  uint8_t ppower;
   double clocks_elapsed, time_elapsed;
   int i, ignore = 0;
 
   uint64_t msizes[] = {1L << 32, 1L << 40, 1L << 52};
-  uint64_t psizes[] = {1L << 12, 1L << 16, 1L << 32};
+  uint8_t psize_powers[] = {12, 16, 32};
+  uint64_t psizes[] = {1L << psize_powers[0], 1L << psize_powers[1], 1L << psize_powers[2]};
+
+  // this is snarky but since this is a contrived example we could just precompute this entirely
+  // and get constant time lookup
+  // uint64_t pagecounts[] = {msizes[0]/psizes[0], msizes[1]/psizes[1], msizes[2]/psizes[2]};
 
   baseline_start = clock();
   for (i = 0; i < TEST_LOOPS; i++)
@@ -33,8 +39,8 @@ int main(int argc, char **argv)
   for (i = 0; i < TEST_LOOPS; i++)
   {
     memory_size = msizes[i % 3];
-    page_size = psizes[i % 3];
-    ignore += pagecount(memory_size, page_size) + memory_size + page_size;
+    ppower = psize_powers[i % 3];
+    ignore += pagecount(memory_size, ppower) + memory_size + ppower;
   }
   test_end = clock();
 
