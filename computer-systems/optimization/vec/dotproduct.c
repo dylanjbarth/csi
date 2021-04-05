bae7633f923c584cd8ed9b9ae6901acd69d18d87
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 18:57:21 -0500

    eliminate loop inefficiency.
    before
    tests.c:86:test_longer:PASS
    0.02s to run test_longer (20648000.00ns per test)
    
    after
    tests.c:86:test_longer:PASS
    0.02s to run test_longer (19368000.00ns per test)

--- computer-systems/optimization/vec/dotproduct.c
+++ computer-systems/optimization/vec/dotproduct.c
@@ -1,13 +1,16 @@
 #include "vec.h"
 
-
-data_t dotproduct(vec_ptr u, vec_ptr v) {
+data_t dotproduct(vec_ptr u, vec_ptr v)
+{
    data_t sum = 0, u_val, v_val;
 
-   for (long i = 0; i < vec_length(u); i++) { // we can assume both vectors are same length
-        get_vec_element(u, i, &u_val);
-        get_vec_element(v, i, &v_val);
-        sum += u_val * v_val;
-   }   
+   long len = vec_length(u); // moved this out oof the loop so it's evaluated once
+
+   for (long i = 0; i < len; i++)
+   { // we can assume both vectors are same length
+      get_vec_element(u, i, &u_val);
+      get_vec_element(v, i, &v_val);
+      sum += u_val * v_val;
+   }
    return sum;
 }
