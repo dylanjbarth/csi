3bc0e0a8dd5dc5b8a6295e6d3092e844a6014970
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 19:21:57 -0500

    Remove procedure calls
    Before:
    tests.c:86:test_longer:PASS
    0.02s to run test_longer (19368000.00ns per test)
    
    After:
    tests.c:86:test_longer:PASS
    0.02s to run test_longer (15648000.00ns per test)

--- computer-systems/optimization/vec/dotproduct.c
+++ computer-systems/optimization/vec/dotproduct.c
@@ -4,12 +4,16 @@ data_t dotproduct(vec_ptr u, vec_ptr v)
 {
    data_t sum = 0, u_val, v_val;
 
-   long len = vec_length(u); // moved this out oof the loop so it's evaluated once
+   long len = vec_length(u); // moved this out of the loop so it's evaluated once, did not affect perf in instruments
+
+   // allow direct data access, removing two procedure calls
+   data_t *ustart = get_vec_start(u);
+   data_t *vstart = get_vec_start(v);
 
    for (long i = 0; i < len; i++)
-   { // we can assume both vectors are same length
-      get_vec_element(u, i, &u_val);
-      get_vec_element(v, i, &v_val);
+   {                     // we can assume both vectors are same length
+      u_val = ustart[i]; // get_vec_element(u, i, &u_val);
+      v_val = vstart[i]; // get_vec_element(v, i, &v_val);
       sum += u_val * v_val;
    }
    return sum;
