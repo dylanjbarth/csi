f8ce5c2789c59d842f200922eaf76e421c822648
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 19:35:23 -0500

    Multiple accumulators.
    Before
    tests.c:86:test_longer:PASS
    0.01s to run test_longer (14103000.00ns per test)
    
    After
    tests.c:86:test_longer:PASS
    0.01s to run test_longer (13564000.00ns per test)

--- computer-systems/optimization/vec/dotproduct.c
+++ computer-systems/optimization/vec/dotproduct.c
@@ -2,7 +2,7 @@
 
 data_t dotproduct(vec_ptr u, vec_ptr v)
 {
-   data_t sum = 0;
+   data_t sum1 = 0, sum2 = 0;
 
    long len = vec_length(u); // moved this out of the loop so it's evaluated once, did not affect perf in instruments
 
@@ -14,13 +14,13 @@ data_t dotproduct(vec_ptr u, vec_ptr v)
    long i;
    for (i = 0; i < len; i += 2)
    { // we can assume both vectors are same length
-      sum += ustart[i] * vstart[i];
-      sum += ustart[i + 1] * vstart[i + 1];
+      sum1 += ustart[i] * vstart[i];
+      sum2 += ustart[i + 1] * vstart[i + 1];
    }
    // finish off the loop
    for (; i < len; i++)
    { // we can assume both vectors are same length
-      sum += ustart[i] * vstart[i];
+      sum1 += ustart[i] * vstart[i];
    }
-   return sum;
+   return sum1 + sum2;
 }
