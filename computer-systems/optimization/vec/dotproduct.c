ef8bac65d65a5e2a4af99882fb7cc440b4f2f6de
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 19:39:59 -0500

    3x3 loop unrolling + multiple accumulators (doesn't seem to improve it further)
    Before:
    tests.c:86:test_longer:PASS
    0.01s to run test_longer (13564000.00ns per test)
    
    tests.c:86:test_longer:PASS
    0.01s to run test_longer (13535000.00ns per test)

--- computer-systems/optimization/vec/dotproduct.c
+++ computer-systems/optimization/vec/dotproduct.c
@@ -2,7 +2,7 @@
 
 data_t dotproduct(vec_ptr u, vec_ptr v)
 {
-   data_t sum1 = 0, sum2 = 0;
+   data_t sum1 = 0, sum2 = 0, sum3 = 0;
 
    long len = vec_length(u); // moved this out of the loop so it's evaluated once, did not affect perf in instruments
 
@@ -10,17 +10,18 @@ data_t dotproduct(vec_ptr u, vec_ptr v)
    data_t *ustart = get_vec_start(u);
    data_t *vstart = get_vec_start(v);
 
-   // 2x1 loop unrolling
+   // 3X3 loop unrolling
    long i;
-   for (i = 0; i < len; i += 2)
+   for (i = 0; i < len; i += 3)
    { // we can assume both vectors are same length
       sum1 += ustart[i] * vstart[i];
       sum2 += ustart[i + 1] * vstart[i + 1];
+      sum3 += ustart[i + 2] * vstart[i + 2];
    }
    // finish off the loop
    for (; i < len; i++)
    { // we can assume both vectors are same length
       sum1 += ustart[i] * vstart[i];
    }
-   return sum1 + sum2;
+   return sum1 + sum2 + sum3;
 }
