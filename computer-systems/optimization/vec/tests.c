ce7ccc09f9e4fd0b531086bcd916322f70001efc
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 19:17:11 -0500

    Add some of the profiling code from pagecount to vec tests

--- computer-systems/optimization/vec/tests.c
+++ computer-systems/optimization/vec/tests.c
@@ -1,16 +1,20 @@
 #include "vendor/unity.h"
 
 #include "vec.h"
+#include <time.h>
 
 extern data_t dotproduct(vec_ptr, vec_ptr);
 
-void setUp(void) {
+void setUp(void)
+{
 }
 
-void tearDown(void) {
+void tearDown(void)
+{
 }
 
-void test_empty(void) {
+void test_empty(void)
+{
   vec_ptr u = new_vec(0);
   vec_ptr v = new_vec(0);
 
@@ -20,7 +24,8 @@ void test_empty(void) {
   free_vec(v);
 }
 
-void test_basic(void) {
+void test_basic(void)
+{
   vec_ptr u = new_vec(3);
   vec_ptr v = new_vec(3);
 
@@ -37,12 +42,14 @@ void test_basic(void) {
   free_vec(v);
 }
 
-void test_longer(void) {
+void test_longer(void)
+{
   long n = 1000000;
   vec_ptr u = new_vec(n);
   vec_ptr v = new_vec(n);
 
-  for (long i = 0; i < n; i++) {
+  for (long i = 0; i < n; i++)
+  {
     set_vec_element(u, i, i + 1);
     set_vec_element(v, i, i + 1);
   }
@@ -54,12 +61,33 @@ void test_longer(void) {
   free_vec(v);
 }
 
-int main(void) {
-    UNITY_BEGIN();
+int main(void)
+{
+  clock_t test_start, test_end;
+  double clocks_elapsed, time_elapsed;
 
-    RUN_TEST(test_empty);
-    RUN_TEST(test_basic);
-    RUN_TEST(test_longer);
+  UNITY_BEGIN();
 
-    return UNITY_END();
+  test_start = clock();
+  RUN_TEST(test_empty);
+  test_end = clock();
+  clocks_elapsed = test_end - test_start;
+  time_elapsed = clocks_elapsed / CLOCKS_PER_SEC;
+  printf("%.2fs to run test_empty (%.2fns per test)\n", time_elapsed, time_elapsed * 1e9);
+
+  test_start = clock();
+  RUN_TEST(test_basic);
+  test_end = clock();
+  clocks_elapsed = test_end - test_start;
+  time_elapsed = clocks_elapsed / CLOCKS_PER_SEC;
+  printf("%.2fs to run test_start (%.2fns per test)\n", time_elapsed, time_elapsed * 1e9);
+
+  test_start = clock();
+  RUN_TEST(test_longer);
+  test_end = clock();
+  clocks_elapsed = test_end - test_start;
+  time_elapsed = clocks_elapsed / CLOCKS_PER_SEC;
+  printf("%.2fs to run test_longer (%.2fns per test)\n", time_elapsed, time_elapsed * 1e9);
+
+  return UNITY_END();
 }
