0151cfaebd3c5d0baf5ba9f97fcffb0dbaed853e
Dylan Barth <dylan.barth@liongard.com>
2021-04-05 19:30:11 -0500

    2x1 loop unrolling
    Before
    tests.c:86:test_longer:PASS
    0.02s to run test_longer (15098000.00ns per test)
    
    After
    tests.c:86:test_longer:PASS
    0.01s to run test_longer (14103000.00ns per test)

--- computer-systems/optimization/vec/dotproduct.c
+++ computer-systems/optimization/vec/dotproduct.c
@@ -10,7 +10,15 @@ data_t dotproduct(vec_ptr u, vec_ptr v)
    data_t *ustart = get_vec_start(u);
    data_t *vstart = get_vec_start(v);
 
-   for (long i = 0; i < len; i++)
+   // 2x1 loop unrolling
+   long i;
+   for (i = 0; i < len; i += 2)
+   { // we can assume both vectors are same length
+      sum += ustart[i] * vstart[i];
+      sum += ustart[i + 1] * vstart[i + 1];
+   }
+   // finish off the loop
+   for (; i < len; i++)
    { // we can assume both vectors are same length
       sum += ustart[i] * vstart[i];
    }
