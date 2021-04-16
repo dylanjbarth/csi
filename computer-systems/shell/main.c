1b5350798f49b84b4296d167194a9a023c8383b9
Dylan Barth <dylan.barth@liongard.com>
2021-04-16 11:44:54 -0500

    Somewhat working with exec and fork.

--- computer-systems/shell/main.c
+++ computer-systems/shell/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #define PROMPT "$> "
 #define BYE "BYEEEEEEEE"
 #define MAXCHAR 100
+#define MAXARGS 5
+
+void split(char *str, char **output);
 
 int main(int argc, char **argv)
 {
@@ -30,6 +36,38 @@ int main(int argc, char **argv)
       i++;
     }
     printf("%s\n", input);
+    int cpid = fork();
+    int cstat;
+    if (cpid > 0)
+    {
+      // parent process, wait for child to complete then reap
+      waitpid(cpid, &cstat, WUNTRACED);
+    }
+    else
+    {
+      // split strings on whitespace into char array
+      // exec if command else command not found
+      char **args = malloc(MAXARGS * sizeof(char *));
+      split(input, args);
+      int err = execvp(args[0], args);
+      if (err < 0)
+      {
+        printf("Exec failed");
+      }
+    }
   };
   return 0;
 };
+
+void split(char *str, char **output)
+{
+  char *ptr;
+  size_t i = 0;
+  ptr = strtok(str, " ");
+  while (ptr != NULL)
+  {
+    output[i] = ptr;
+    ptr = strtok(NULL, " ");
+    i++;
+  }
+}
