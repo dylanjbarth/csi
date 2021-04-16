80de67111d65848de1c39bd9a7ac9c00f49bf77a
Dylan Barth <dylan.barth@liongard.com>
2021-04-15 19:35:00 -0500

    exiting and echoing.

--- computer-systems/shell/main.c
+++ computer-systems/shell/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define PROMPT "$> "
+#define BYE "BYEEEEEEEE"
 #define MAXCHAR 100
 
 int main(int argc, char **argv)
 {
-  char input[MAXCHAR];
   while (1)
   {
+    char input[MAXCHAR] = "";
+    char ch;
     printf("%s", PROMPT);
-    fgets(input, MAXCHAR, stdin);
-    printf("%s", input);
+    int i = 0;
+    while ((ch = fgetc(stdin)))
+    {
+      if (ch == EOF)
+      {
+        printf("^D\n%s\n", BYE);
+        exit(0);
+      }
+      else if (ch == '\n')
+      {
+        break;
+      }
+      input[i] = ch;
+      i++;
+    }
+    printf("%s\n", input);
   };
   return 0;
 };
