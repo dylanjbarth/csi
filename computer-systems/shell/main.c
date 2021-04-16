c68fb08ae9503564f22cb925a491756e495b224b
Dylan Barth <dylan.barth@liongard.com>
2021-04-15 19:19:17 -0500

    echo shell.

--- computer-systems/shell/main.c
+++ computer-systems/shell/main.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+
+#define PROMPT "$> "
+#define MAXCHAR 100
 
 int main(int argc, char **argv)
 {
+  char input[MAXCHAR];
+  while (1)
+  {
+    printf("%s", PROMPT);
+    fgets(input, MAXCHAR, stdin);
+    printf("%s", input);
+  };
   return 0;
 };
