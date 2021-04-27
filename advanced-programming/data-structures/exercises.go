eafe145aca3b480bd8765bfdd6ff5dd91717df45
Dylan Barth <dylan.barth@liongard.com>
2021-04-26 21:01:42 -0500

    try to compare but sleepy and can't figure out conditions rn.

--- advanced-programming/data-structures/exercises.go
+++ advanced-programming/data-structures/exercises.go
@@ -1,6 +1,7 @@
 package exercises
 
 import (
+	"fmt"
 	"unsafe"
 )
 
@@ -14,20 +15,16 @@ func float64ToUint64Bin(n float64) uint64 {
 func stringsShareMemory(s1, s2 *string) bool {
 	// strings are 2 words each, 1st word is pointer to memory location of underlying byte array...
 	// so need to figure out how to access that pointer?
-	// s1ptr := unsafe.Pointer(s1) // this is a pointer to the start of the 2 word string in memory (which itself is a pointer to the underlying data)
-	// // s1len := unsafe.Pointer(uintptr(s1ptr) + unsafe.Sizeof(s1ptr))
-	// s2ptr := unsafe.Pointer(s2)
-	// return s1ptr == s2ptr
-	// s2len := unsafe.Pointer(uintptr(s2ptr) + unsafe.Sizeof(s2ptr))
-	// fmt.Println(*(*string)(s1ptr))
-	// fmt.Println(*(*int)(s1len))
-	// s2ptr := unsafe.Pointer(&s2)
-	// s1ptr := unsafe.Pointer(&s1)
-	// s2ptr := unsafe.Pointer(&s2)
-	// return s1ptr == s1len
-	// but this still works too?
-	return s1 == s2
-	// TODO think the actual point of this exercise is to see if any of the memory overlaps, not just the start of the string data!
+	s1ptr := unsafe.Pointer(s1) // this is a pointer to the start of the 2 word string in memory (which itself is a pointer to the underlying data)
+	s1len := unsafe.Pointer(uintptr(s1ptr) + unsafe.Sizeof(s1ptr))
+	s1start := uintptr(s1ptr)
+	s1end := uintptr(s1ptr) + uintptr(s1len)*unsafe.Sizeof(s1ptr)
+	s2ptr := unsafe.Pointer(s2)
+	s2len := unsafe.Pointer(uintptr(s2ptr) + unsafe.Sizeof(s2ptr))
+	s2start := uintptr(s2ptr)
+	s2end := uintptr(s2ptr) + uintptr(s2len)*unsafe.Sizeof(s2ptr)
+	fmt.Printf("Test case s1 %s start %d end %d s2 %s start %d end %d\n", *s1, s1start, s1end, *s2, s2start, s2end)
+	return (s1start <= s2start && s1end >= s2start) || (s1start <= s2end && s1end >= s2end)
 }
 
 func sumSlice(n *[]int) int {
