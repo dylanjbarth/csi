f0894b689b9b781f2f91e2d63f3d9ed47d6720a3
Dylan Barth <dylan.barth@liongard.com>
2021-05-04 07:45:58 -0500

    solution for example-6

--- advanced-programming/concurrency/debugging/example-6.go
+++ advanced-programming/concurrency/debugging/example-6.go
@@ -17,11 +17,15 @@ func newCoordinator(leader string) *coordinator {
 	}
 }
 
+// logs state without acquiring a lock -- must acquire lock before calling this!
+func (c *coordinator) logStateUnsafe() {
+	fmt.Printf("leader = %q\n", c.leader)
+}
+
 func (c *coordinator) logState() {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
-
-	fmt.Printf("leader = %q\n", c.leader)
+	c.logStateUnsafe()
 }
 
 func (c *coordinator) setLeader(leader string, shouldLog bool) {
@@ -31,7 +35,7 @@ func (c *coordinator) setLeader(leader string, shouldLog bool) {
 	c.leader = leader
 
 	if shouldLog {
-		c.logState()
+		c.logStateUnsafe()
 	}
 }
 
@@ -40,3 +44,21 @@ func main() {
 	c.logState()
 	c.setLeader("us-west", true)
 }
+
+/*
+Problem:
+
+$ go run -race example-6.go
+leader = "us-east"
+^Csignal: interrupt
+
+deadlock. setLeader locks the mutex for reading and writing, and then shouldLog attempts to acquire a lock for reading prior to releasing the outer lock.
+
+Solution:
+
+one way around this is to refactor to decouple lock acquisition from the business logic of logging.
+
+$ go run -race example-6.go
+leader = "us-east"
+leader = "us-west"
+*/
