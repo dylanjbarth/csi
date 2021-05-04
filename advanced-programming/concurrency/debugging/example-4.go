ee2fd7f8d51e601e1b7ec4dc96302a772ab3e07a
Dylan Barth <dylan.barth@liongard.com>
2021-05-04 07:09:06 -0500

    solution for example-4

--- advanced-programming/concurrency/debugging/example-4.go
+++ advanced-programming/concurrency/debugging/example-4.go
@@ -8,9 +8,28 @@ func main() {
 	done := make(chan struct{}, 1)
 	go func() {
 		fmt.Println("performing initialization...")
-		<-done
+		done <- struct{}{}
 	}()
 
-	done <- struct{}{}
+	<-done
 	fmt.Println("initialization done, continuing with rest of program")
 }
+
+/*
+
+$ go run -race example-4.go
+initialization done, continuing with rest of program
+performing initialization...
+
+Problems:
+
+- intended initialization happening _after_ continuation because we are sending to the channel outside the goroutine and receiving inside (so not blocking via the channel as intended)
+
+Solution:
+
+- swap the send and receive on the channel so that we send an event to the done channel when init completes and receive it before continuing execution
+
+$ go run -race example-4.go
+performing initialization...
+initialization done, continuing with rest of program
+*/
