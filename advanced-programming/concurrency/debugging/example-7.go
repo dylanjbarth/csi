8a569a9b0ec0f0ab6b82522091e4217f7a381e08
Dylan Barth <dylan.barth@liongard.com>
2021-05-04 07:55:24 -0500

    solution for example-7

--- advanced-programming/concurrency/debugging/example-7.go
+++ advanced-programming/concurrency/debugging/example-7.go
@@ -14,7 +14,7 @@ type counter struct {
 	count int
 }
 
-func safeIncrement(lock sync.Mutex, c *counter) {
+func safeIncrement(lock *sync.Mutex, c *counter) {
 	lock.Lock()
 	defer lock.Unlock()
 
@@ -34,7 +34,7 @@ func main() {
 			defer wg.Done()
 
 			for j := 0; j < numIncrements; j++ {
-				safeIncrement(globalLock, c)
+				safeIncrement(&globalLock, c)
 			}
 		}()
 	}
@@ -42,3 +42,43 @@ func main() {
 	wg.Wait()
 	fmt.Println(c.count)
 }
+
+/*
+
+Problem
+
+$ go run -race example-7.go
+==================
+WARNING: DATA RACE
+Read at 0x00c000138010 by goroutine 8:
+  main.safeIncrement()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:21 +0xbe
+  main.main.func1()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:37 +0xa5
+
+Previous write at 0x00c000138010 by goroutine 7:
+  main.safeIncrement()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:21 +0xd7
+  main.main.func1()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:37 +0xa5
+
+Goroutine 8 (running) created at:
+  main.main()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:33 +0x129
+
+Goroutine 7 (finished) created at:
+  main.main()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-7.go:33 +0x129
+==================
+9654
+Found 1 data race(s)
+exit status 66
+
+Solution:
+
+- pass by value by default in golang, so we were creating a copy of the lock each time we pass it in.
+- turn this into a pointer so we are sharing the same lock between goroutines.
+
+$ go run -race example-7.go
+10000
+*/
