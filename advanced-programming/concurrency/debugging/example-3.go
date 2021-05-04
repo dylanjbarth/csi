b54657835b0730ee83ce9ebbf721e4cce914d3ce
Dylan Barth <dylan.barth@liongard.com>
2021-05-04 07:06:18 -0500

    solution for example-3

--- advanced-programming/concurrency/debugging/example-3.go
+++ advanced-programming/concurrency/debugging/example-3.go
@@ -14,8 +14,8 @@ func main() {
 	}
 	var wg sync.WaitGroup
 	for i := range urls {
+		wg.Add(1)
 		go func(i int) {
-			wg.Add(1)
 			// Decrement the counter when the goroutine completes.
 			defer wg.Done()
 
@@ -32,3 +32,51 @@ func main() {
 	wg.Wait()
 	fmt.Println("all url fetches done!")
 }
+
+/*
+Initial output:
+
+$ go run -race example-3.go
+==================
+WARNING: DATA RACE
+Write at 0x00c000138288 by main goroutine:
+  internal/race.Write()
+      /usr/local/go/src/internal/race/race.go:41 +0x125
+  sync.(*WaitGroup).Wait()
+      /usr/local/go/src/sync/waitgroup.go:128 +0x126
+  main.main()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-3.go:32 +0x18e
+
+Previous read at 0x00c000138288 by goroutine 7:
+  internal/race.Read()
+      /usr/local/go/src/internal/race/race.go:37 +0x206
+  sync.(*WaitGroup).Add()
+      /usr/local/go/src/sync/waitgroup.go:71 +0x219
+  main.main.func1()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-3.go:18 +0x5e
+
+Goroutine 7 (running) created at:
+  main.main()
+      /Users/dylanbarth/projects/csi/advanced-programming/concurrency/debugging/example-3.go:17 +0x170
+==================
+Successfully fetched https://bradfieldcs.com/courses/databases/
+Successfully fetched https://bradfieldcs.com/courses/architecture/
+Successfully fetched https://bradfieldcs.com/courses/networking/
+all url fetches done!
+Found 1 data race(s)
+exit status 66
+
+Problems:
+
+- we are incrementing the wg inside the goroutine (async) instead of before we start the goroutine, causing a race condition.
+
+Solution:
+
+- move wg.add call outside of the goroutine (but inside the loop)
+
+$ go run -race example-3.go
+Successfully fetched https://bradfieldcs.com/courses/architecture/
+Successfully fetched https://bradfieldcs.com/courses/databases/
+Successfully fetched https://bradfieldcs.com/courses/networking/
+all url fetches done!
+*/
