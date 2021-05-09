fd7a0db4ff82f5cecb81af32df8ce66bf088b965
Dylan Barth <dylan.barth@liongard.com>
2021-05-08 19:03:39 -0500

    Add failing concurrent test case.

--- advanced-programming/mutexes/mutex_test.go
+++ advanced-programming/mutexes/mutex_test.go
@@ -22,4 +22,28 @@ func TestFootex(t *testing.T) {
 		t.Error("Expected Unlock to unlock the token.")
 	}
 
+	// Test concurrent access to shared var.
+	n_test := 1000
+	d := uint32(0)
+	results := make(chan uint32, n_test)
+	for i := 0; i < n_test; i++ {
+		go func() {
+			tok.Lock()
+			d += 1
+			results <- d
+			tok.Unlock()
+		}()
+	}
+	// Ensure that we incremented correctly and have the right max
+	var max uint32
+	for i := 0; i < n_test; i++ {
+		r := <-results
+		if r > max {
+			max = r
+		}
+	}
+	expected := uint32(n_test) - 1
+	if max != expected {
+		t.Errorf("Expected max value to equal %d but got %d", expected, max)
+	}
 }
