983e372fc17d97108902fb2a0c1188a474139903
Dylan Barth <dylan.barth@liongard.com>
2021-05-09 11:55:18 -0500

    Slow but working!

--- advanced-programming/mutexes/mutex.go
+++ advanced-programming/mutexes/mutex.go
@@ -2,7 +2,6 @@ package mutex
 
 import (
 	"bytes"
-	"fmt"
 	"runtime"
 	"strconv"
 	"sync/atomic"
@@ -25,39 +24,51 @@ type footex struct {
 // https://pkg.go.dev/github.com/davecheney/junk/id
 // If you use this package, you will go straight to hell. LOL
 // Pure go version https://blog.sgmansfield.com/2015/12/goroutine-ids/
-// func unsafeGetGoroutineId() uint64 {
-// 	b := make([]byte, 64)
-// 	b = b[:runtime.Stack(b, false)]
-// 	b = bytes.TrimPrefix(b, []byte("goroutine "))
-// 	b = b[:bytes.IndexByte(b, ' ')]
-// 	n, _ := strconv.ParseUint(string(b), 10, 64)
-// 	return n
-// }
+func unsafeGetGoroutineId() uint64 {
+	b := make([]byte, 64)
+	b = b[:runtime.Stack(b, false)]
+	// fmt.Printf("Stack: %s\n", b)
+	// NB this dumps something like:
+	/*
+		goroutine 8 [running]:
+		ap/mutexes.unsafeGetGoroutineId(0x0)
+			/Us
+		Stack: goroutine 8 [running]:
+		ap/mutexes.unsafeGetGoroutineId(0xc00005c
+		Stack: goroutine 7 [running]:
+		ap/mutexes.unsafeGetGoroutineId(0x0)
+			/Us
+		Stack: goroutine 7 [running]:
+		ap/mutexes.unsafeGetGoroutineId(0xc00005c
+	*/
+	// And we take the ID of the calling goroutine.
+	// Under the hood, runtime.Stack is calling getg => func getg() *g, and formatting this into
+	// goroutine + gp.goid + [status]
+	// Interesting that goroutines are identity-less from a public perspective but of course they have an ID under the hood!
+	b = bytes.TrimPrefix(b, []byte("goroutine "))
+	b = b[:bytes.IndexByte(b, ' ')]
+	n, _ := strconv.ParseUint(string(b), 10, 64)
+	return n
+}
 
 func (f *footex) Lock() {
 	// if lock owner is nil, the caller gets the lock, otherwise continue to try
-	pc, _, _, ok := runtime.Caller(1) // get caller ptr
-	if !ok {
-		panic("Unable to determine ptr to caller of Lock")
-	}
-	fmt.Printf("Called from %d\n", pc)
+	gid := unsafeGetGoroutineId()
+	// fmt.Printf("Called from %d\n", gid)
 	for {
-		if atomic.CompareAndSwapUintptr(&f.owner, nptr, pc) {
-			fmt.Println("Locked")
+		if atomic.CompareAndSwapUintptr(&f.owner, nptr, uintptr(gid)) {
+			// fmt.Println("Locked")
 			return
 		}
 	}
 }
 
 func (f *footex) Unlock() {
-	pc, _, _, ok := runtime.Caller(1)
-	if !ok {
-		panic("Unable to determine caller ptr")
-	}
-	fmt.Printf("Called from %d\n", pc)
+	gid := unsafeGetGoroutineId()
+	// fmt.Printf("Called from %d\n", gid)
 	for {
-		if atomic.CompareAndSwapUintptr(&f.owner, pc, nptr) {
-			fmt.Println("Unlocked")
+		if atomic.CompareAndSwapUintptr(&f.owner, uintptr(gid), nptr) {
+			// fmt.Println("Unlocked")
 			return
 		}
 	}
--- advanced-programming/mutexes/mutex_test.go
+++ advanced-programming/mutexes/mutex_test.go
@@ -8,19 +8,19 @@ func TestFootex(t *testing.T) {
 	tok := footex{}
 
 	// Test concurrent access to shared var.
-	n_test := 2
+	n_test := 1000
 	d := uint32(0)
 	results := make(chan uint32, n_test)
 	for i := 0; i < n_test; i++ {
-		t.Logf("Starting %d\n", i)
+		// t.Logf("Starting %d\n", i)
 		go func(n int) {
-			t.Logf("%d calling lock\n", n)
+			// t.Logf("%d calling lock\n", n)
 			tok.Lock()
 			d += 1
 			results <- d
-			t.Logf("%d calling unlock\n", n)
+			// t.Logf("%d calling unlock\n", n)
 			tok.Unlock()
-			t.Logf("%d returning\n", n)
+			// t.Logf("%d returning\n", n)
 		}(i)
 	}
 	// Ensure that we incremented correctly and have the right max
@@ -31,7 +31,7 @@ func TestFootex(t *testing.T) {
 			max = r
 		}
 	}
-	expected := uint32(n_test) - 1
+	expected := uint32(n_test)
 	if max != expected {
 		t.Errorf("Expected max value to equal %d but got %d", expected, max)
 	}
