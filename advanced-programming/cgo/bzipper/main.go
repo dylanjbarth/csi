260cb78b8fb9664b56ddc427a1bc783a01e46c4a
Dylan Barth <dylan.barth@liongard.com>
2021-05-22 09:29:14 -0500

    ensuring the examples for bzip work from the book (gopl.io)

---
+++ advanced-programming/cgo/bzipper/main.go
@@ -0,0 +1,28 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 365.
+
+//!+
+
+// Bzipper reads input, bzip2-compresses it, and writes it out.
+package main
+
+import (
+	"ap/cgo/bzip"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	w := bzip.NewWriter(os.Stdout)
+	if _, err := io.Copy(w, os.Stdin); err != nil {
+		log.Fatalf("bzipper: %v\n", err)
+	}
+	if err := w.Close(); err != nil {
+		log.Fatalf("bzipper: close: %v\n", err)
+	}
+}
+
+//!-
