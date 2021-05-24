b7c26a0c260ebec74846235bb56916832de1e3b0
Dylan Barth <dylan.barth@liongard.com>
2021-05-24 07:18:17 -0500

    Open/close working.

--- advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
+++ advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
@@ -6,19 +6,44 @@ package ldbwrap
 // #include "leveldb/c.h"
 import "C"
 import (
+	"errors"
 	"fmt"
 	"unsafe"
 )
 
-func Open(path string) {
+type LevelDB struct {
+	path   string
+	isOpen bool
+	cptr   *C.leveldb_t
+}
+
+func NewLevelDB(path string) *LevelDB {
+	return &LevelDB{path: path, isOpen: false}
+}
+
+func (ldb *LevelDB) Open() (*LevelDB, error) {
+	if ldb.isOpen {
+		return nil, errors.New("database is already open")
+	}
 	errptr := C.CString("")
 	opts := C.leveldb_options_create()
 	C.leveldb_options_set_create_if_missing(opts, C.uchar(1))
-	C.leveldb_open(opts, C.CString(path), &errptr)
-	if C.GoString(errptr) != "" {
-		panic(C.GoString(errptr))
-	} else {
-		fmt.Printf("Connected to db %s", path)
-	}
+	ldb.cptr = C.leveldb_open(opts, C.CString(ldb.path), &errptr)
+	err := C.GoString(errptr)
 	C.free(unsafe.Pointer(errptr))
+	if err != "" {
+		return nil, fmt.Errorf("failed to connect to DB. error is: %s", err)
+	}
+	ldb.isOpen = true
+	return ldb, nil
+}
+
+func (ldb *LevelDB) Close() (*LevelDB, error) {
+	if !ldb.isOpen {
+		return nil, errors.New("database is already closed")
+	}
+	C.leveldb_close(ldb.cptr)
+	ldb.isOpen = false
+	ldb.cptr = nil
+	return ldb, nil
 }
--- advanced-programming/cgo/leveldb-wrapper/ldbwrap_test.go
+++ advanced-programming/cgo/leveldb-wrapper/ldbwrap_test.go
@@ -2,10 +2,23 @@ package ldbwrap
 
 import "testing"
 
-func TestOpen(t *testing.T) {
-	Open("/tmp/test_level_db")
+func TestOpenClose(t *testing.T) {
+	ldb := NewLevelDB("/tmp/test_level_db")
+	if ldb.isOpen {
+		t.Error("Expected new levelDB to be closed after init and before open.")
+	}
+	ldb.Open()
+	if !ldb.isOpen {
+		t.Error("Expected levelDB to be open after calling open.")
+	}
+	t.Logf("Pointer to db connection is %v", ldb.cptr)
+	ldb.Close()
+	if ldb.isOpen {
+		t.Error("Expected levelDB to be closed after calling close.")
+	}
+	t.Logf("Pointer to db connection is %v", ldb.cptr)
 }
 
-func TestBadPathOpen(t *testing.T) {
-	Open("/blah/test_level_db")
-}
+// func TestBadPathOpen(t *testing.T) {
+// 	Open("/blah/test_level_db")
+// }
