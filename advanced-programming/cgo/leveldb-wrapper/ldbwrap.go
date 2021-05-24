bd332580d2495a5548cf0e324158070975a39de9
Dylan Barth <dylan.barth@liongard.com>
2021-05-24 06:54:50 -0500

    Open & bad open tests working with err pointer.

--- advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
+++ advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
@@ -2,14 +2,23 @@ package ldbwrap
 
 // #cgo CFLAGS: -I/usr/local/Cellar/leveldb/1.23/include
 // #cgo LDFLAGS: -L/usr/local/Cellar/leveldb/1.23/lib -lleveldb
+// #include <stdlib.h>
 // #include "leveldb/c.h"
 import "C"
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
-func Open() {
-	db := C.CString("test")
+func Open(path string) {
+	errptr := C.CString("")
 	opts := C.leveldb_options_create()
 	C.leveldb_options_set_create_if_missing(opts, C.uchar(1))
-	status := C.leveldb_open(opts, C.CString("/tmp/test_level_db"), &db)
-	fmt.Println(*status)
+	C.leveldb_open(opts, C.CString(path), &errptr)
+	if C.GoString(errptr) != "" {
+		panic(C.GoString(errptr))
+	} else {
+		fmt.Printf("Connected to db %s", path)
+	}
+	C.free(unsafe.Pointer(errptr))
 }
--- advanced-programming/cgo/leveldb-wrapper/ldbwrap_test.go
+++ advanced-programming/cgo/leveldb-wrapper/ldbwrap_test.go
@@ -3,5 +3,9 @@ package ldbwrap
 import "testing"
 
 func TestOpen(t *testing.T) {
-	Open()
+	Open("/tmp/test_level_db")
+}
+
+func TestBadPathOpen(t *testing.T) {
+	Open("/blah/test_level_db")
 }
