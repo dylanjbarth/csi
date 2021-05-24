5ba3c785a97c959277447e6a3a2cb7c9fe45723d
Dylan Barth <dylan.barth@liongard.com>
2021-05-23 20:10:35 -0500

    One step forward two steps back...
    fixed the linker and now cgo is defining leveldb_open at least, but the types are still empty structs.

--- advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
+++ advanced-programming/cgo/leveldb-wrapper/ldbwrap.go
@@ -1,30 +1,23 @@
 package ldbwrap
 
 // #cgo CFLAGS: -I/usr/local/Cellar/leveldb/1.23/include
-// #cgo LDFLAGS: -L/usr/local/Cellar/leveldb/1.23/lib
+// #cgo LDFLAGS: -L/usr/local/Cellar/leveldb/1.23/lib -lleveldb
 // #include "leveldb/c.h"
 import "C"
+import "fmt"
 
 func Init() {
-	// TODO why are these empty structs?
-	var db C.leveldb_t
-	var opts C.leveldb_options_t
+	// TODO why are these generated as empty structs by cgo?
+	// var db *C.leveldb_t  // cannot use _cgo2 (type **_Ctype_struct_leveldb_t) as type **_Ctype_char in argument to _Cfunc_leveldb_open
+	db := C.CString("test")
+	opts := C.leveldb_options_t{} // ./ldbwrap.go:13:2: _Ctype_struct_leveldb_options_t is incomplete (or unallocatable); stack allocation disallowed
 	// this isn't allowed since this is an empty struct...
-	opts.create_if_missing = true
-	/*
-		$ go build
-		# ldbwrap
-		Undefined symbols for architecture x86_64:
-			"_leveldb_open", referenced from:
-					__cgo_67271468db4f_Cfunc_leveldb_open in _x002.o
-				(maybe you meant: __cgo_67271468db4f_Cfunc_leveldb_open)
-		ld: symbol(s) not found for architecture x86_64
-		clang: error: linker command failed with exit code 1 (use -v to see invocation)
-	*/
-	status := C.leveldb_open(opts, "/tmp/test_level_db", &db)
-	if !status.ok() {
-		panic("Unable to open database")
-	}
+	// opts.create_if_missing = true
+	status := C.leveldb_open(&opts, C.CString("/tmp/test_level_db"), &db)
+	fmt.Println(status)
+	// if !status.ok() {
+	// 	panic("Unable to open database")
+	// }
 }
 
 func Open() {
