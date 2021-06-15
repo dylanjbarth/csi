63c347a1ba45c72a4fa4e03ec5d9230ac79d51e1
Dylan Barth <dylan.barth@liongard.com>
2021-06-14 20:24:43 -0400

    clean shutdown when connection terminates.

--- networking/http/main.go
+++ networking/http/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"syscall"
 )
 
-const maxConn, srcPort, dstPort = 10, 8001, 8002
+const maxConn, srcPort, dstPort, maxBytes = 10, 8001, 8002, 1500
+const cachePath = "cachethis/" // TODO obviously cleaner to allow this to be passed in
 
 func main() {
-	// create socket to listen on
-	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
-	if err != nil {
-		log.Fatalf("unable to create socket. %s", err)
-	}
-	// bind socket to source port
-	err = syscall.Bind(fd, &syscall.SockaddrInet4{Port: srcPort, Addr: [4]byte{127, 0, 0, 1}})
-	if err != nil {
-		log.Fatalf("unable to bind socket. %s", err)
-	}
+	emptyBytes := make([]byte, maxBytes)
+	// cache := make(map[string][]byte)
 
+	// connect proxy to our end server
 	destServer := syscall.SockaddrInet4{Port: dstPort, Addr: [4]byte{127, 0, 0, 1}}
-
-	// set up socket for communication with destination server
 	sfd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 	if err != nil {
 		log.Fatalf("unable to create socket. %s", err)
 	}
-	// establish a connection to our destination server
 	err = syscall.Connect(sfd, &destServer)
 	if err != nil {
 		log.Fatalf("unable to connect to dst server. %s", err)
 	}
 
-	// Set socket up to listen for connections
+	// create socket to listen for clients on and bind it to our source port
+	fd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		log.Fatalf("unable to create socket. %s", err)
+	}
+	err = syscall.Bind(fd, &syscall.SockaddrInet4{Port: srcPort, Addr: [4]byte{127, 0, 0, 1}})
+	if err != nil {
+		log.Fatalf("unable to bind socket. %s", err)
+	}
 	err = syscall.Listen(fd, maxConn)
 	if err != nil {
 		log.Fatalf("listen failed. %s", err)
 	}
-
 	// accept first incoming connection
 	nfd, addr, aerr := syscall.Accept(fd)
 	if aerr != nil {
@@ -46,29 +46,43 @@ func main() {
 	log.Printf("Accepted conn from %+v", addr)
 
 	for {
-		b := make([]byte, 1500) //https://stackoverflow.com/a/2614188 assuming ethernet packet here
-		n, frm, rerr := syscall.Recvfrom(nfd, b, 0)
+		b := make([]byte, maxBytes) //https://stackoverflow.com/a/2614188 assuming ethernet packet here
+		_, _, rerr := syscall.Recvfrom(nfd, b, 0)
 		if rerr != nil {
 			log.Fatalf("recvfrom failed. %s", rerr)
 		}
-		log.Printf("%d, %+v", n, frm)
 		log.Printf("Message received from client: %s", b)
-		log.Printf("Sending these bytes to our dest server")
-		serr := syscall.Sendto(sfd, b, 0, &destServer)
-		if serr != nil {
-			log.Fatalf("send failed. %s", serr)
+		// The return value will be 0 when the peer has performed an orderly shutdown.
+		if bytes.Equal(emptyBytes, b) {
+			log.Print("Got empty response from client, indicating orderly shutdown.")
+			break
 		}
-		// Get the response and forward it back to the original requester
-		n, frm, rerr = syscall.Recvfrom(sfd, b, 0)
-		if rerr != nil {
-			log.Fatalf("recvfrom failed. %s", rerr)
-		}
-		log.Printf("%d, %+v", n, frm)
-		log.Printf("Response from dest server: %s", b)
-		log.Printf("Sending these bytes to our client")
-		serr = syscall.Sendto(nfd, b, 0, addr)
-		if serr != nil {
-			log.Fatalf("send failed. %s", serr)
+		val := fmt.Sprintf("%s", b)
+		if val != "" {
+			log.Printf("Sending these bytes to our dest server")
+			serr := syscall.Sendto(sfd, b, 0, &destServer)
+			if serr != nil {
+				log.Fatalf("send failed. %s", serr)
+			}
+			// Get the response and forward it back to the original requester
+			_, _, rerr = syscall.Recvfrom(sfd, b, 0)
+			if rerr != nil {
+				log.Fatalf("recvfrom failed. %s", rerr)
+			}
+			log.Printf("Response from dest server: %s", b)
+			log.Printf("Sending these bytes to our client")
+			serr = syscall.Sendto(nfd, b, 0, addr)
+			if serr != nil {
+				log.Fatalf("send failed. %s", serr)
+				// TODO getting a broken pipe error here when the client closes the connection..
+				// but the bytes are getting through to the client. some flag I should be setting on the sendto?
+			}
+		} else {
+			log.Printf("Message from client was empty so closing the connection.")
+			err = syscall.Shutdown(nfd, syscall.SHUT_RDWR)
+			if err != nil {
+				log.Fatalf("shutdown failed. %s", err)
+			}
 		}
 	}
 }
