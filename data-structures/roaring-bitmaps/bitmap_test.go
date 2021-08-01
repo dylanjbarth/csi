8ee75ab034576b6470c92e3a35ba3ca9164b473a
Dylan Barth <dylan.barth@liongard.com>
2021-08-01 18:50:21 -0400

    Compress working (maybe?)

--- data-structures/roaring-bitmaps/bitmap_test.go
+++ data-structures/roaring-bitmaps/bitmap_test.go
@@ -1,6 +1,7 @@
 package bitmap
 
 import (
+	"fmt"
 	"math/rand"
 	"testing"
 )
@@ -57,12 +58,12 @@ func TestBitmap(t *testing.T) {
 	// TODO: Uncomment this section once you get compression / decompression working
 	compressed := compress(b1)
 	t.Logf("Uncompressed size: %d words, compressed size: %d words\n", len(b1.data), len(compressed))
-	b := decompress(compressed)
-	for x := uint32(0); x < start+limit+wordSize; x++ {
-		if b1.Get(x) != b.Get(x) {
-			t.Fatalf("Compression then decompression produced inconsistent result for %d\n", x)
-		}
-	}
+	// b := decompress(compressed)
+	// for x := uint32(0); x < start+limit+wordSize; x++ {
+	// 	if b1.Get(x) != b.Get(x) {
+	// 		t.Fatalf("Compression then decompression produced inconsistent result for %d\n", x)
+	// 	}
+	// }
 }
 
 func TestGetNextChunk(t *testing.T) {
@@ -78,4 +79,14 @@ func TestGetNextChunk(t *testing.T) {
 	if second != 20 {
 		t.Errorf("Expected second 63 bits to equal 22 but got %d", second)
 	}
+}
+
+func TestCompress(t *testing.T) {
+	b1 := newUncompressedBitmap()
+	b1.Set(1)
+	b1.Set(3)
+	b1.Set(65)
+	compressed := compress(b1)
+	fmt.Printf("Before %b %b %b", b1.data[0], b1.data[2], b1.data[3])
+	fmt.Printf("After %b %b", compressed[0], compressed[2])
 }
--- data-structures/roaring-bitmaps/rle.go
+++ data-structures/roaring-bitmaps/rle.go
@@ -1,17 +1,33 @@
 package bitmap
 
-import "fmt"
+import (
+	// "fmt"
+	"math"
+)
 
 // Word Aligned Hybrid encoding, 1 signal - 63 following bits.
 func compress(b *uncompressedBitmap) []uint64 {
-	// n_runs := 0
-	// var out []uint64
-	chunk := getNext63Bits(b, 0)
-	// iterations = len(b.data) / 64
-	for i := 0; i < len(b.data); i++ {
-
+	var out []uint64
+	n_runs := 0
+	offset := uint64(0)
+	chunksize := 63
+	n_iterations := int(math.Ceil(float64(len(b.data) / chunksize)))
+	for i := 0; i < n_iterations; i++ {
+		chunk := getNext63Bits(b, offset)
+		if chunk == 0 {
+			n_runs += 1
+			offset += uint64(chunksize)
+			continue
+		} else if n_runs > 0 {
+			// flush n_runs before processing this chunk.
+			out = append(out, uint64(n_runs))
+			n_runs = 0
+		}
+		// flip signal bit and then write the rest of the 63 bits here
+		out = append(out, chunk|1<<63)
+		offset += uint64(chunksize)
 	}
-	return []uint64{chunk}
+	return out
 }
 
 func decompress(compressed []uint64) *uncompressedBitmap {
@@ -31,31 +47,31 @@ func getNext63Bits(b *uncompressedBitmap, bitoffset uint64) uint64 {
 	s_rem := start % 64
 	e_idx := end / 64
 	e_rem := end % 64
-	fmt.Printf("Bitoffset, start, end: %d %d %d \n", bitoffset, start, end)
-	fmt.Printf("Start index, end index: %d %d \n", s_idx, e_idx)
-	fmt.Printf("Start remainder, end remainder: %d %d \n", s_rem, e_rem)
+	// fmt.Printf("Bitoffset, start, end: %d %d %d \n", bitoffset, start, end)
+	// fmt.Printf("Start index, end index: %d %d \n", s_idx, e_idx)
+	// fmt.Printf("Start remainder, end remainder: %d %d \n", s_rem, e_rem)
 	if s_idx == e_idx { // in this rare situation, we can just extract the element and clean off the starting or ending bit.
-		fmt.Printf("Single element approach:\n")
-		fmt.Printf("Element: %b\n", b.data[s_idx])
+		// fmt.Printf("Single element approach:\n")
+		// fmt.Printf("Element: %b\n", b.data[s_idx])
 		if s_rem == 0 { // clear end bit
 			val = b.data[s_idx] >> 1
 		} else { // clear starting bit
 			val = b.data[s_idx] &^ (1 << 63)
 		}
-		fmt.Printf("Val: %b\n", val)
+		// fmt.Printf("Val: %b\n", val)
 	} else { // in the more common situation we have to join bits from two adjacent indexes, clearing bits from both
-		fmt.Printf("Two element approach:\n")
+		// fmt.Printf("Two element approach:\n")
 		first := b.data[s_idx] << s_rem
 		second := uint64(0)
 		if len(b.data)-1 <= int(e_idx) {
 			second = b.data[e_idx] >> e_rem
 		}
 		val = first | second
-		fmt.Printf("First: %b\n", b.data[s_idx])
-		fmt.Printf("First shifted: %b\n", first)
-		fmt.Printf("Second: %b\n", b.data[e_idx])
-		fmt.Printf("Second shifted: %b\n", second)
-		fmt.Printf("Val: %b\n", val)
+		// fmt.Printf("First: %b\n", b.data[s_idx])
+		// fmt.Printf("First shifted: %b\n", first)
+		// fmt.Printf("Second: %b\n", b.data[e_idx])
+		// fmt.Printf("Second shifted: %b\n", second)
+		// fmt.Printf("Val: %b\n", val)
 	}
 	return val
 }
