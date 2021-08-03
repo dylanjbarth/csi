d9670dfca614f49ccd2b28dacb4e873de65f759f
Dylan Barth <dylan.barth@liongard.com>
2021-08-03 08:06:10 -0400

    Rewrite implementation to not preallocate initial slice.

--- data-structures/roaring-bitmaps/uncompressed.go
+++ data-structures/roaring-bitmaps/uncompressed.go
@@ -7,13 +7,19 @@ type uncompressedBitmap struct {
 }
 
 func newUncompressedBitmap() *uncompressedBitmap {
-	// 2^32 / 64 => max capacity of the bitmap
-	return &uncompressedBitmap{data: make([]uint64, 67_108_864)}
+	// Original approach was to pre-allocate, but this is really slow to compress if it's not needed...
+	// 2^32 / 64 => max capacity of the bitmap, 8MB
+	// return &uncompressedBitmap{data: make([]uint64, 67_108_864)}
+	// Try empty strategy so that compress is faster
+	return &uncompressedBitmap{data: []uint64{}}
 }
 
 func (b *uncompressedBitmap) Get(x uint32) bool {
 	idx := x / 64
 	bitToCheck := x % 64
+	if int(idx) > len(b.data)-1 {
+		return false
+	}
 	return b.data[idx]&(1<<bitToCheck) > 0
 }
 
@@ -21,24 +27,43 @@ func (b *uncompressedBitmap) Set(x uint32) {
 	// Identify the bitmap index (where the decimal value is the index of the bit to flip)
 	idx := x / 64
 	bitToSet := x % 64
+	// Extend length of slice if needed.
+	diff := int(idx) - len(b.data) + 1
+	if diff > 0 {
+		b.data = append(b.data, make([]uint64, diff)...)
+	}
 	// Set the bit by taking the bitwise OR of the existing value and the bit that we need to set.
 	b.data[idx] = b.data[idx] | (1 << bitToSet)
 }
 
-// TODO Union & intersection would be more annoying if we didn't know the bitmaps were the same length...
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
+
 func (b *uncompressedBitmap) Union(other *uncompressedBitmap) *uncompressedBitmap {
-	var data = make([]uint64, len(b.data))
-	for i := 0; i < len(b.data); i++ {
+	minL := min(len(b.data), len(other.data))
+	var data = make([]uint64, minL)
+	for i := 0; i < minL; i++ {
 		data[i] = b.data[i] | other.data[i]
 	}
+	// fill in the rest
+	largest := b
+	if len(b.data) < len(other.data) {
+		largest = other
+	}
+	data = append(data, (largest.data[minL:])...)
 	return &uncompressedBitmap{
 		data: data,
 	}
 }
 
 func (b *uncompressedBitmap) Intersect(other *uncompressedBitmap) *uncompressedBitmap {
-	var data = make([]uint64, len(b.data))
-	for i := 0; i < len(b.data); i++ {
+	minL := min(len(b.data), len(other.data))
+	var data = make([]uint64, minL)
+	for i := 0; i < minL; i++ {
 		data[i] = b.data[i] & other.data[i]
 	}
 	return &uncompressedBitmap{
