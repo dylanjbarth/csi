664bc18d055792192efc33d483483736bc923c56
Dylan Barth <dylan.barth@liongard.com>
2021-10-29 17:01:50 -0400

    Fix tests.

--- distributed-systems/kvstore/kv/parser_test.go
+++ distributed-systems/kvstore/kv/parser_test.go
@@ -8,35 +8,29 @@ func TestParser(t *testing.T) {
 	if err != nil {
 		t.Errorf("Expected parse to not fail but got %s", err)
 	}
-	if out.Cmd != "get" {
-		t.Errorf("Expected parsed command to be get but got %s", out.Cmd)
+	if out.Command != Request_GET {
+		t.Errorf("Expected parsed command to be get but got %s", out.Command)
 	}
-	if len(out.Args) != 1 {
-		t.Errorf("Expected arguments to be len 1 but got %d", len(out.Args))
-	}
-	if out.Args[0] != "foo" {
-		t.Errorf("Expected parsed command to be foo but got %s", out.Args[0])
+	if out.Item.Key != "foo" {
+		t.Errorf("Expected parsed command to be foo but got %s", out.Item.Key)
 	}
 
 	out, err = p.Parse("set foo=bar")
 	if err != nil {
 		t.Errorf("Expected parse to not fail but got %s", err)
 	}
-	if out.Cmd != "set" {
-		t.Errorf("Expected parsed command to be set but got %s", out.Cmd)
+	if out.Command != Request_SET {
+		t.Errorf("Expected parsed command to be set but got %s", out.Command)
 	}
-	if len(out.Args) != 2 {
-		t.Errorf("Expected arguments to be len 2 but got %d", len(out.Args))
+	if out.Item.Key != "foo" {
+		t.Errorf("Expected parsed arg to be foo but got %s", out.Item.Key)
 	}
-	if out.Args[0] != "foo" {
-		t.Errorf("Expected parsed arg to be foo but got %s", out.Args[0])
-	}
-	if out.Args[1] != "bar" {
-		t.Errorf("Expected parsed arg to be bar but got %s", out.Args[1])
+	if out.Item.Value != "bar" {
+		t.Errorf("Expected parsed arg to be bar but got %s", out.Item.Value)
 	}
 
 	out, err = p.Parse("foo")
 	if err == nil {
-		t.Errorf("Expected parse to fail but got %s", out)
+		t.Errorf("Expected parse to fail but got %s", &out)
 	}
 }
